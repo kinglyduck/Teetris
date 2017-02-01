89b7a700865d9a769da6da121a2e857915d7b716
Nicholas Palmerley <NicholasPalmerley@hotmail.com>
2017-02-01 10:18:33 -0800

    Worked on Player Movement

--- Assets/Player2Movement.cs
+++ Assets/Player2Movement.cs
@@ -2,28 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour {
-
-	public int v = 100;
-
+public class Player2Movement : MonoBehaviour {
+	public int v = 10;
 	// Use this for initialization
 	void Start () {
-
-
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
-		if (Input.GetKey (KeyCode.A)) {
+		if (Input.GetKey (KeyCode.LeftArrow)) {
 			transform.position += v * Time.deltaTime * Vector3.left;
 		}
-		if (Input.GetKey (KeyCode.D)) {
+		if (Input.GetKey (KeyCode.RightArrow)) {
 			transform.position += v * Time.deltaTime * Vector3.right;
 		}
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			transform.position += v * Time.deltaTime * Vector3.up;
+		}
 
-		transform.position += 5 * Time.deltaTime * Vector3.down;
-		
 	}
 }
